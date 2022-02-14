d771820b89c6887bbb3307b3c6d1e9322590e9dd
Israel David <77063876+israel1281@users.noreply.github.com>
2022-02-14 09:43:16 +0000

    init commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+        "BASE_URL": "https://nextjs-eshop-1h96odfws-israel1281.vercel.app",
+        "MONGODB_URL": "mongodb+srv://kidoski1:waldav125@cluster0.5wmyr.mongodb.net/eshop?retryWrites=true&w=majority  ",
+        "ACCESS_TOKEN_SECRET": "ectron123",
+        "REFRESH_TOKEN_SECRET": "ectron456",
+        "PAYPAL_CLIENT_ID": "ARCZixKkeK-wHCaVnhDtjQBSwh6_J9sJt7jR0YJdDWKcXcFGLL3JG8UvL0arWYkGDu0FFYPgAPZl8v_b",
+        "CLOUD_UPDATE_PRESET": "nextjs_store",
+        "CLOUD_NAME": "devat-channel",
+        "CLOUD_API": "https://api.cloudinary.com/v1_1/devat-channel/image/upload"
+    }
 }
