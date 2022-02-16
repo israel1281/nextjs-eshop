d10f9200539696eb2848be559940686850248a90
Israel David <77063876+israel1281@users.noreply.github.com>
2022-02-16 02:47:22 +0000

    init commit

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, {Html, Head, Main, NextScript} from 'next/document'
+import Script from 'next/script'
 
 class MyDocument extends Document{
   render(){
@@ -7,10 +8,10 @@ class MyDocument extends Document{
          <Head>
                     <meta name="description" content="Dev AT E-commerce website with Next.js"/>
                     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" />
-                    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
-                    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-                    <script src={`https://www.paypal.com/sdk/js?client-id=${process.env.PAYPAL_CLIENT_ID}`}></script>
+                    <Script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></Script>
+                    <Script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></Script>
+                    <Script src="https://kit.fontawesome.com/a076d05399.js"></Script>
+                    <Script src={`https://www.paypal.com/sdk/js?client-id=${process.env.PAYPAL_CLIENT_ID}`}></Script>
                 </Head>
                 <body>
                     <Main />
